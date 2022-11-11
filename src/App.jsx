100b54c6b1deed30276e685a9945e9e915ea1ac0
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-11-11 20:14:22 -0300

    initial config

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Base from './components/Base';
+import Header from './components/Header';
+import Home from './components/Home';
+import Order from './components/Order';
+import Toppings from './components/Toppings';
+
+function App() {
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
+
+  const addBase = (base) => {
+    setPizza({ ...pizza, base })
+  }
+  
+  const addTopping = (topping) => {
+    let newToppings;
+    if(!pizza.toppings.includes(topping)){
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter(item => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  }
+
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/base">
+          <Base addBase={addBase} pizza={pizza} />
+        </Route>
+        <Route path="/toppings">
+          <Toppings addTopping={addTopping} pizza={pizza} />
+        </Route>
+        <Route path="/order">
+          <Order pizza={pizza} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
