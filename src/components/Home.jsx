af54a6387072ddb77955fca388b8f029863b5fe3
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-11-11 23:37:40 -0300

    feat(base): init with keyframes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,14 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const buttonVariants = {
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    textShadow: '0px 0px 8px rgb(255, 255, 255)',
+    boxShadow: '0px 0px 8px rgb(255, 255, 255)'
+  }
+}
+
 const Home = () => {
   return (
     <motion.div 
@@ -15,11 +23,8 @@ const Home = () => {
       </h2>
       <Link to="/base">
         <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: '0px 0px 8px rgb(255, 255, 255)',
-            boxShadow: '0px 0px 8px rgb(255, 255, 255)'
-          }}
+          variants={buttonVariants}
+          whileHover="hover"
         >
           Create Your Pizza
         </motion.button>
