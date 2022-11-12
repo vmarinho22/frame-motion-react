1fcc25e97c36ea6ee067cf01b9a63c65713c43a3
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-11-11 21:09:07 -0300

    feat(base): basic hover animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,13 @@ const Home = () => {
         Welcome to Pizza Joint
       </h2>
       <Link to="/base">
-        <motion.button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: '0px 0px 8px rgb(255, 255, 255)',
+            boxShadow: '0px 0px 8px rgb(255, 255, 255)'
+          }}
+        >
           Create Your Pizza
         </motion.button>
       </Link>
