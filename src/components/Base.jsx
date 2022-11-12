2803198d6726cf1779e00a3da8f71cf9adb50301
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-11-11 21:24:12 -0300

    feat(base): init with basic variants props

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -2,19 +2,40 @@ import { motion } from "framer-motion";
 import React from "react";
 import { Link } from "react-router-dom";
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw"
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring", 
+      delay: 0.5
+    }
+  }
+}
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw"
+  },
+  visible: {
+    x: 0
+  },
+  transition: { type: "spring", stiffness: 120 }
+}
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
     <motion.div
       className="base container"
-      initial={{
-        x: "100vw"
-      }}
-      animate={{
-        x: 0
-      }}
-      transition={{ type: "spring", delay: 0.5 }}
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -43,13 +64,7 @@ const Base = ({ addBase, pizza }) => {
       {pizza.base && (
         <motion.div
           className="next"
-          initial={{
-            x: "-100vw"
-          }}
-          animate={{
-            x: 0
-          }}
-          transition={{ type: "spring", stiffness: 120 }}
+          variants={nextVariants}
         >
           <Link to="/toppings">
             <motion.button
