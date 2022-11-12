c38fe554d8fef8099328cc4ff3528d8ebae02594
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-11-11 23:52:15 -0300

    feat(base): using animation presence

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
-import React from 'react';
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from 'react';
 
 const containerVariants = {
   hidden: {
@@ -29,6 +29,12 @@ const childVariants = {
 }
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true);
+
+  setTimeout(() => {
+    setShowTitle(false);
+  }, 4000)
+  
   return (
     <motion.div 
       className="container order"
@@ -36,7 +42,13 @@ const Order = ({ pizza }) => {
       initial="hidden"
       animate="visible"
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2
+            exit={{ y: - 1000 }}
+          >Thank you for your order :)</motion.h2>
+        )}
+      </AnimatePresence>
       <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
       <motion.div variants={childVariants}>
         {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
